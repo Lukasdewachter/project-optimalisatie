652273d1a664fea6a10db43919788096dfa4c07b
lukas <lukas.dewachter@student.kuleuven.be>
2022-11-14 15:41:45 +0100

    fix lib

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import org.json.*;
+import java.io.*;
+import java.util.*;
+import org.json.JSONTokener;
+import org.json.simple.parser.JSONParser;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
+        
     }
 }
